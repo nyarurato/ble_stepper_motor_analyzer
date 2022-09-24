6db87ffe4f5456a60be1c5efeb078103f8d1e2a1
Zapta <zapta@users.noreply.github.com>
2022-09-24 10:22:18 -0700

    Added additional debug markers to probe.py

--- python/probe.py
+++ python/probe.py
@@ -53,14 +53,20 @@ class Probe:
         return self.__probe_info
 
     async def __find_service_or_disconnect(self, name: str, uuid: str) -> (BleakGATTService | None):
+        print("** Debug marker __find_service_or_disconnect.01", flush=True)
         if not self.__client.is_connected:
+            print("** Debug marker __find_service_or_disconnect.02", flush=True)
             logger.error(f"Not connected (__find_service_or_disconnect).")
             # return None
+        print("** Debug marker __find_service_or_disconnect.03", flush=True)
         service = self.__client.services.get_service(uuid)
+        print("** Debug marker __find_service_or_disconnect.04", flush=True)
         if not service:
+            print("** Debug marker __find_service_or_disconnect.05", flush=True)
             logger.error(
                 f"Failed to find service {name} at device {self.address()}.")
             await self.disconnect()
+            print("** Debug marker __find_service_or_disconnect.06", flush=True)
             return None
         logger.info(f"Found {name} info service {service}.")
         return service
@@ -90,9 +96,10 @@ class Probe:
 
     @classmethod
     async def find_by_address(cls, dev_addr: str, steps_per_unit: float, timeout: float = 30.0) -> Optional[Probe]:
-        print("** Debug marker 20.1, dev_addr={dev_addr} ", flush=True)
+        print(f"** Debug marker 20.1, dev_addr={dev_addr} ", flush=True)
         device = await BleakScanner.find_device_by_address(dev_addr, timeout=timeout)
         if not device:
+            print(f"** Debug marker 20.1a, dev_addr={dev_addr} ", flush=True)
             logger.error(f"Device with address {dev_addr} not found.")
             return None
         logger.info(f"Found device: {device.address}.")
@@ -107,70 +114,97 @@ class Probe:
         return self.__client.is_connected
 
     async def connect(self, timeout: float = 20.0) -> bool:
+        print("** Debug marker 10.1.100", flush=True)
         if self.is_connected():
+            print("** Debug marker 10.1.101", flush=True)
             return True
 
+        print("** Debug marker 10.1.100a", flush=True)
         await self.__client.connect(timeout=timeout)
+        print("** Debug marker 10.1.102", flush=True)
         if not self.is_connected():
+            print("** Debug marker 10.1.103", flush=True)
             logger.error(f"Failed to connect to {self.address()}.")
             return False
 
         for s in self.__client.services.services.values():
             logger.info(f"* Service: {s}")
 
+        print("** Debug marker 10.1.104", flush=True)
         device_info_service = await self.__find_service_or_disconnect("Device Info", "180a")
         if not device_info_service:
+            print("** Debug marker 10.1.105", flush=True)
             return False
 
+        print("** Debug marker 10.1.106", flush=True)
         stepper_service = await self.__find_service_or_disconnect("Stepper", "68e1a034-8125-4525-8a30-8799018c4bd0")
         if not stepper_service:
+            print("** Debug marker 10.1.107", flush=True)
             return False
 
+        print("** Debug marker 10.1.108", flush=True)
         model_number_bytes = await self.__read_chrc_or_disconnect(device_info_service, "Model Number", "2A24")
         if not model_number_bytes:
+            print("** Debug marker 10.1.109", flush=True)
             return False
 
         manufacturer_bytes = await self.__read_chrc_or_disconnect(device_info_service, "Manufacturer", "2A29")
         if not manufacturer_bytes:
             return False
 
+        print("** Debug marker 10.1.110", flush=True)
         probe_info_bytes = await self.__read_chrc_or_disconnect(stepper_service, "Probe Info", "37e75add-a610-448d-9fd3-3e3130e2c7f2")
         if not probe_info_bytes:
+            print("** Debug marker 10.1.111", flush=True)
             return False
 
         # Get stepper state characteristic.
+        print("** Debug marker 10.1.112", flush=True)
         stepper_state_chrc = await self.__find_chrc_or_disconnect(stepper_service, "Stepper State", "37e75add-a610-448d-9fd3-3e3130e2c7f1")
         if not stepper_state_chrc:
+            print("** Debug marker 10.1.113", flush=True)
             return False
 
         # Get current histogram characteristic.
+        print("** Debug marker 10.1.114", flush=True)
         stepper_current_histogram_chrc = await self.__find_chrc_or_disconnect(stepper_service, "Current Histogram", "37e75add-a610-448d-9fd3-3e3130e2c7f3")
         if not stepper_current_histogram_chrc:
+            print("** Debug marker 10.1.115", flush=True)
             return False
 
         # Get time histogram characteristic.
+        print("** Debug marker 10.1.116", flush=True)
         stepper_time_histogram_chrc = await self.__find_chrc_or_disconnect(stepper_service, "Time Histogram", "37e75add-a610-448d-9fd3-3e3130e2c7f4")
         if not stepper_time_histogram_chrc:
+            print("** Debug marker 10.1.117", flush=True)
             return False
 
         # Get distance histogram characteristic.
+        print("** Debug marker 10.1.118", flush=True)
         stepper_distance_histogram_chrc = await self.__find_chrc_or_disconnect(stepper_service, "Distance Histogram", "37e75add-a610-448d-9fd3-3e3130e2c7f5")
         if not stepper_distance_histogram_chrc:
+            print("** Debug marker 10.1.119", flush=True)
             return False
 
         # Get stepper command characteristic.
+        print("** Debug marker 10.1.120", flush=True)
         stepper_command_chrc = await self.__find_chrc_or_disconnect(stepper_service, "Command", "37e75add-a610-448d-9fd3-3e3130e2c7f6")
         if not stepper_command_chrc:
+            print("** Debug marker 10.1.121", flush=True)
             return False
 
         # Get capture signal characteristic.
+        print("** Debug marker 10.1.122", flush=True)
         capture_signal_chrc = await self.__find_chrc_or_disconnect(stepper_service, "Command", "37e75add-a610-448d-9fd3-3e3130e2c7f7")
         if not capture_signal_chrc:
+            print("** Debug marker 10.1.123", flush=True)
             return False
 
         # Set this object.
+        print("** Debug marker 10.1.124", flush=True)
         self.__probe_info = ProbeInfo.decode(
             probe_info_bytes, model_number_bytes.decode(), manufacturer_bytes.decode())
+        print("** Debug marker 10.1.125", flush=True)
         self.__stepper_state_chrc = stepper_state_chrc
         self.__stepper_current_histogram_chrc = stepper_current_histogram_chrc
         self.__stepper_time_histogram_chrc = stepper_time_histogram_chrc
@@ -178,7 +212,9 @@ class Probe:
         self.__stepper_command_chrc = stepper_command_chrc
         self.__capture_signal_chrc = capture_signal_chrc
 
+        print("** Debug marker 10.1.116", flush=True)
         logger.info(f"Connected to {self.address()}.")
+        print("** Debug marker 10.1.127", flush=True)
         return True
 
     def info(self) -> (ProbeInfo | None):
